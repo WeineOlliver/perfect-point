826f1070580c45f9cc9e87e0cbed1a5dd5af7b44
Weine Oliveira <weine.santos.de.oliveira@ibm.com>
2020-02-01 16:22:55 -0300

    twilio integration, and dotenv setup

--- src/index.js
+++ src/index.js
@@ -1,37 +1,58 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const puppeeteer = require('puppeteer');
+const client = require('twilio')(
+  process.env.TWILIO_SID,
+  process.env.TWILIO_TOKEN
+);
 
-const pointUrl = 'https://www.ahgora.com.br/batidaonline';
+const pointUrl = process.env.PONTO_URL;
+const key = process.env.PONTO_WEB_IDENTITY_KEY;
+const value = process.env.PONTO_WEB_IDENTITY_VALUE;
 
 const perfectPoint = async () => {
-  const browser = await puppeeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(pointUrl);
-  await page.evaluate(() => {
-    localStorage.setItem(
-      'pontoweb_identity',
-      'bdef3818d0b4a650ce3a1bf04049c622'
+  try {
+    const browser = await puppeeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(pointUrl);
+    await page.evaluate(
+      ({ key, value }) => {
+        localStorage.setItem(key, value);
+      },
+      { key, value }
     );
-  });
-  await page.goto(pointUrl, {
-    waitUntil: 'networkidle0'
-  });
-  await page.on('response', async response => {
-    const { result } = await response.json();
-    if (!result) {
-      console.log('errow');
-    }
-  });
-  await page.click('input#account_i');
-  await page.keyboard.type('50318', {
-    delay: 15
-  });
-  await page.click('input#password_i');
-  await page.keyboard.type('50318xzxzxz', {
-    delay: 15
-  });
-  await page.click('button#botao_entrar');
+    await page.goto(pointUrl, {
+      waitUntil: 'networkidle0'
+    });
+    await page.on('response', async response => {
+      const { result } = await response.json();
+      if (!result) {
+        client.messages.create({
+          body: 'Perfect point failed, please verify manually',
+          from: process.env.TWILIO_NUMBER,
+          to: process.env.MY_PHONE_NUMBER
+        });
+      } else {
+        client.messages.create({
+          body: 'Perfect point had success!',
+          from: process.env.TWILIO_NUMBER,
+          to: process.env.MY_PHONE_NUMBER
+        });
+      }
+    });
+    await page.click('input#account_i');
+    await page.keyboard.type(process.env.PONTO_USER, {
+      delay: 15
+    });
+    await page.click('input#password_i');
+    await page.keyboard.type('xzxzxz', {
+      delay: 15
+    });
+    await page.click('button#botao_entrar');
+  } catch (error) {
+    console.log(error);
+  }
 };
-
 const init = () => {
   perfectPoint();
 };
